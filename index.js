6986dbeb9c6d6efd5e5b1a8017d9db5d363ff163
sudo-apt-get-updates <70002587+sudo-apt-get-updates@users.noreply.github.com>
2021-01-27 22:27:27 -0500

    Ability to startup, fixes to opts

--- index.js
+++ index.js
@@ -6,51 +6,78 @@ var Queue = require('bull');
 
 module.exports = (sails) => {
   return {
-    initialize: async function() {
+    initialize: async function () {
       let queueDir = path.join(sails.config.appPath, 'api/queues')
-      if(!fs.existsSync(queueDir)) {
-        return;  
+      if (!fs.existsSync(queueDir)) {
+        return;
       }
-      
+
       let queueFiles = fs.readdirSync(queueDir).filter(fn => /\.js$/.test(fn))
 
-      for(let file of queueFiles) {
+      for (let file of queueFiles) {
         let filePath = path.join(queueDir, file)
-        
-        let queue = buildQueue.bind(this)(sails, filePath)
+
+        let obj = buildQueue.bind(this)(sails, filePath);
+        if (obj == null) {
+          continue;
+        }
+        let queue = obj.queue;
+        let queueData = obj.queueData;
+
+        if (queue == null) {
+          continue;
+        }
+
+        if (queue.name == null) {
+          continue;
+        }
 
         let queueFile = path.basename(filePath, '.js')
 
         //Make queue accesible
-        this[queueFile] = queue
+        this[queueFile] = queue;
+        if (queueData.startup) {
+          if (queueData.opts) {
+            this[queueFile].add(`${queueData.queueName}-process`, {}, queueData.opts);
+          }
+        }
+
       }
     }
   }
 };
 
 
-const buildQueue = function(sails, filePath) {
+const buildQueue = function (sails, filePath) {
   let queueData = getDataFromPath.bind(this)(sails, filePath)
-  
+
+  if (queueData == null) {
+    return null;
+  }
+
   let queue = createQueue(queueData)
-  
+
+  if (queue == null) {
+    return null;
+  }
+
   //Create process if available
-  if('process' in queueData) {
+  if ('process' in queueData) {
     createProcess(queue, queueData)
   }
 
-  return queue
+  return {queue: queue, queueData: queueData};
 }
 
-const getDataFromPath = function(sails, filePath) {
+const getDataFromPath = function (sails, filePath) {
   let queueData = require(filePath)
-  
+  getDataFromPath
   let isObject = queueData !== null && typeof queueData === 'object'
-  if(!isObject) {
-    throw new Error(`${filePath} should export an object`)
+  if (!isObject) {
+    return null;
   }
-  
-  //Merge queue options with config/bull.js
+
+  // Merge queue options with config/bull.js
   Object.assign(queueData, {opts: _.assign(sails.config[this.configKey], queueData.opts)})
 
   return queueData;
@@ -60,10 +87,19 @@ const getDataFromPath = function(sails, filePath) {
 /**
  * Create queue from file definition
  */
-const createQueue = function(queueData) {
+const createQueue = function (queueData) {
+
+  if (queueData == null) {
+    return null;
+  }
+
+  if (queueData.queueName == null) {
+    return null;
+  }
+
   //Pick args in order
-  let args = _.pick(queueData, ['queueName', 'url', 'opts'])
-      args = Object.values(args)
+  let args = _.pick(queueData, ['queueName', 'url', 'opts', 'startup'])
+  args = Object.values(args)
 
   return Queue(...args)
 }
@@ -73,11 +109,11 @@ const createQueue = function(queueData) {
  * Create process for a queue
  */
 const createProcess = (queue, queueData) => {
-  if(typeof queueData.process==="function") {
+  if (typeof queueData.process === "function") {
     queue.process(queueData.process)
-  }else {
+  } else {
     let args = _.pick(queueData.process, ['name', 'concurrency', 'processor'])
-        args = Object.values(args)
+    args = Object.values(args)
 
     queue.process(...args)
   }
